505682b8072aabffeafa72415bfeb816526ceb9d
jyesares <javiyesares@gmail.com>
2018-06-17 21:55:37 +0200

    change backend to serve the SPA

--- server/boot/root.js
+++ server/boot/root.js
@@ -1,8 +1,10 @@
 'use strict';
 
 module.exports = function(server) {
-  // Install a `/` route that returns server status
-  var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
-  server.use(router);
+  if (process.env.NODE_ENV !== 'production') {
+    // Install a `/` route that returns server status
+    var router = server.loopback.Router();
+    router.get('/', server.loopback.status());
+    server.use(router);
+  }
 };
