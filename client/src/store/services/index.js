afcba2e883499b7c15b1506bd032288f61b05fdf
jyesares <javiyesares@gmail.com>
2018-06-18 19:01:52 +0200

    retrieve date upload and ownername

--- client/src/store/services/index.js
+++ client/src/store/services/index.js
@@ -1,2 +1,4 @@
 export const getRecentPhotos = page =>
-  fetch(`/api/photos/getrecents?per_page=10&page=${page}`);
+  fetch(
+    `/api/photos/getrecents?per_page=10&page=${page}&extras=owner_name,date_upload`
+  );
