32455e73147d61d4f5c17233c48ba9614412988f
jyesares <javiyesares@gmail.com>
2018-06-18 20:19:37 +0200

    elipse if there are more than 60 chars in the title

--- client/src/components/ImageDescription.js
+++ client/src/components/ImageDescription.js
@@ -3,22 +3,27 @@ import moment from 'moment';
 
 import './styles.css';
 
-export default ({photo}) => (
-  <div className="description">
-    <span>
-      <b>Title</b>: {photo.title}
-    </span>
-    <span>
-      <b>ID</b>: {photo.id}
-    </span>
-    <span>
-      <b>OwnerId</b>: {photo.owner}
-    </span>
-    <span>
-      <b>OwnerName</b>: {photo.ownername}
-    </span>
-    <span>
-      <b>Date upload</b>: {moment.unix(photo.dateupload).format('MM/DD/YYYY')}
-    </span>
-  </div>
-);
+export default ({photo}) => {
+  const {title, id, owner, ownername, dateupload} = photo;
+
+  return (
+    <div className="description">
+      <span>
+        <b>Title</b>:{' '}
+        {title.length >= 60 ? `${title.substring(0, 59)}...` : title}
+      </span>
+      <span>
+        <b>ID</b>: {id}
+      </span>
+      <span>
+        <b>OwnerId</b>: {owner}
+      </span>
+      <span>
+        <b>OwnerName</b>: {ownername}
+      </span>
+      <span>
+        <b>Date upload</b>: {moment.unix(dateupload).format('MM/DD/YYYY')}
+      </span>
+    </div>
+  );
+};
