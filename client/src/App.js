20c18953a5d673c3d4b4e2e66d7151a5411036ae
jyesares <javiyesares@gmail.com>
2018-06-18 19:01:25 +0200

    add header and title

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+
+import './App.css';
 import Photos from './components/Photos';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="App-header">
+          <div className="App-title">Gallery</div>
+        </div>
         <Photos />
       </div>
     );
