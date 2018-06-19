3e493796e1cd6dbe92698cb30db1a65604a7699d
jyesares <javiyesares@gmail.com>
2018-06-19 14:41:22 +0200

    redirect to flicker owner page instead of farm

--- client/src/components/Image.js
+++ client/src/components/Image.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 export default ({photo, index, onClick, enableLink = false}) => {
-  const {farm, server, id, secret} = photo;
+  const {farm, server, id, secret, owner} = photo;
   const url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}.jpg`;
+  const urlFlickr = `https://www.flickr.com/photos/${owner}/${id}`;
   return (
     <div>
       {enableLink ? (
-        <a href={enableLink === true ? url : ''}>
+        <a href={enableLink === true ? urlFlickr : ''}>
           <img key={index || 0} src={url} onClick={onClick} />
         </a>
       ) : (
